dad6d34fe34ee9887d3af0693eebf034de45c20b
itkr <itkrst@gmail.com>
2015-10-29 20:25:16 +0900

    Update setup.py

--- jpmynumber/__init__.py
+++ jpmynumber/__init__.py
@@ -1,2 +1,4 @@
 # -*- coding:utf-8 -*-
 from .core import JPMyNumber
+
+__version__ = '0.0.1'
--- setup.py
+++ setup.py
@@ -1,12 +1,21 @@
+import os
 from setuptools import setup
+
+from jpmynumber import __version__
+
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst'), 'r') as f:
+    long_description = f.read()
+
 
 setup(
     name='JPMyNumber',
-    version='0.0.1',
+    version=__version__,
     author='itkr',
     author_email='itkrst@gmail.com',
     description=(
         'MyNumber (Japanese common number of social security and tax) library'),
+    long_description=long_description,
     license='MIT',
     keywords=['MyNumber', 'Japanese'],
     url='https://github.com/itkr/JPMyNumber',
