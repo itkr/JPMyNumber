6c656d6d9132d60a506a240e979673027880326c
itkr <itkrst@gmail.com>
2015-10-29 15:40:03 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 from setuptools import setup
 
 setup(
-    name="JPMyNumber",
-    version="0.0.1",
-    author="itkr",
-    author_email="itkrst@gmail.com",
-    description=("MyNumber (Japanese common number of social security and tax) library"),
-    license="MIT",
-    keywords=["MyNumber", "Japanese"],
-    url="https://github.com/itkr/JPMyNumber",
+    name='JPMyNumber',
+    version='0.0.1',
+    author='itkr',
+    author_email='itkrst@gmail.com',
+    description=(
+        'MyNumber (Japanese common number of social security and tax) library'),
+    license='MIT',
+    keywords=['MyNumber', 'Japanese'],
+    url='https://github.com/itkr/JPMyNumber',
     packages=['jpmynumber'],
 )
