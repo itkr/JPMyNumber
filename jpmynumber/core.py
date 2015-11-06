9f81366dd10da15c19def7bd1583ac29c875ac86
itkr <itkrst@gmail.com>
2015-11-06 16:30:33 +0900

    Create Pattern Class

--- jpmynumber/core.py
+++ jpmynumber/core.py
@@ -6,11 +6,28 @@ from jpmynumber.exceptions import (JPMyNumberCheckDigitError,
                                    JPMyNumberPatternError)
 
 
-def _assert_n(func):
+def assert_n(func):
     def validate(self, n):
         assert 1 <= n < self.LEN
         return func(self, n)
     return validate
+
+
+class Pattern(list):
+
+    def copy(self):
+        return Pattern([i for i in self])
+
+    def check(self, index, n):
+        if self[index] is None:
+            return True
+        if hasattr(self[index], '__iter__'):
+            return n in self[index]
+        return n == self[index]
+
+    def update(self, index, n):
+        self[index] = n
+        return self
 
 
 class _ValidationMixin(object):
@@ -24,7 +41,7 @@ class _ValidationMixin(object):
             raise JPMyNumberCheckDigitError
 
     def validate_pattern(self):
-        if not self.check_pattern(self.get_pattern()):
+        if not self.check_pattern(self.pattern):
             raise JPMyNumberPatternError
 
     def validate(self):
@@ -52,7 +69,7 @@ class _CreationMixin(object):
 
     def _update_pattern(self):
         arr = self._to_a
-        for i, pattern_n in enumerate(self.get_pattern()):
+        for i, pattern_n in enumerate(self.pattern):
             if pattern_n is not None:
                 arr[i] = random.choice(pattern_n) if hasattr(
                     pattern_n, '__iter__') else pattern_n
@@ -75,8 +92,8 @@ class JPMyNumber(_CreationMixin, _ValidationMixin):
             number=self.number)
 
     @property
-    def _check_digit_index(self):
-        return self.LEN - 1
+    def pattern(self):
+        return Pattern([None for _i in range(self.LEN)])
 
     @property
     def check_digit(self):
@@ -88,6 +105,10 @@ class JPMyNumber(_CreationMixin, _ValidationMixin):
         remainder = sum([self._f(n)
                          for n in range(1, self.LEN)]) % user_number_len
         return 0 if remainder <= 1 else user_number_len - remainder
+
+    @property
+    def _check_digit_index(self):
+        return self.LEN - 1
 
     @property
     def _to_a(self):
@@ -103,28 +124,20 @@ class JPMyNumber(_CreationMixin, _ValidationMixin):
         del arr[self._check_digit_index]
         return arr
 
-    @_assert_n
+    @assert_n
     def _p(self, n):
         return self._user_number_to_a[-n]
 
-    @_assert_n
+    @assert_n
     def _q(self, n):
         return n + 1 if n <= (self.LEN / 2) else n - 5
 
-    @_assert_n
+    @assert_n
     def _f(self, n):
         return self._p(n) * self._q(n)
 
-    def get_pattern(self):
-        return [None for _i in range(self.LEN)]
-
     def check_pattern(self, pattern):
         for i, n in enumerate(self._to_a):
-            if pattern[i] is None:
-                continue
-            if hasattr(pattern[i], '__iter__'):
-                if n not in pattern[i]:
-                    return False
-            elif pattern[i] != n:
+            if not pattern.check(i, n):
                 return False
         return True
--- jpmynumber/legalentity.py
+++ jpmynumber/legalentity.py
@@ -1,6 +1,6 @@
 # -*- coding:utf-8 -*-
 
-from .core import JPMyNumber, _assert_n
+from .core import JPMyNumber, assert_n
 
 
 class LegalEntityNumber(JPMyNumber):
@@ -8,29 +8,30 @@ class LegalEntityNumber(JPMyNumber):
     LEN = 13
 
     @property
+    def true_check_digit(self):
+        return 9 - sum([self._f(n) for n in range(1, self.LEN)]) % 9
+
+    @property
     def _check_digit_index(self):
         return 0
 
-    @_assert_n
+    @assert_n
     def _q(self, n):
-        return 1 if n % 2 else 2
-
-    @property
-    def true_check_digit(self):
-        return 9 - sum([self._f(n) for n in range(1, self.LEN)]) % 9
+        return n % 2 or 2
 
 
 class StateOrgansNumber(LegalEntityNumber):
 
     """C00001XXXXXXX　国家機関."""
 
-    def get_pattern(self):
-        pattern = super(StateOrgansNumber, self).get_pattern()
-        pattern[1] = 0
-        pattern[2] = 0
-        pattern[3] = 0
-        pattern[4] = 0
-        pattern[5] = 1
+    @property
+    def pattern(self):
+        pattern = super(StateOrgansNumber, self).pattern.copy()
+        pattern.update(1, 0)
+        pattern.update(2, 0)
+        pattern.update(3, 0)
+        pattern.update(4, 0)
+        pattern.update(5, 1)
         return pattern
 
 
@@ -38,13 +39,14 @@ class LocalGovernmentNumber(LegalEntityNumber):
 
     """C0000[2-3]XXXXXXX　地方公共団体."""
 
-    def get_pattern(self):
-        pattern = super(LocalGovernmentNumber, self).get_pattern()
-        pattern[1] = 0
-        pattern[2] = 0
-        pattern[3] = 0
-        pattern[4] = 0
-        pattern[5] = [2, 3]
+    @property
+    def pattern(self):
+        pattern = super(LocalGovernmentNumber, self).pattern.copy()
+        pattern.update(1, 0)
+        pattern.update(2, 0)
+        pattern.update(3, 0)
+        pattern.update(4, 0)
+        pattern.update(5, [2, 3])
         return pattern
 
 
@@ -52,20 +54,20 @@ class LocalGovernmentNumberWithCode(LocalGovernmentNumber):
 
     """C00002XXXXXXX　地方公共団体（団体コードあり）."""
 
-    def get_pattern(self):
-        pattern = super(LocalGovernmentNumberWithCode, self).get_pattern()
-        pattern[5] = 2
-        return pattern
+    @property
+    def pattern(self):
+        pattern = super(LocalGovernmentNumberWithCode, self).pattern.copy()
+        return pattern.update(5, 2)
 
 
 class LocalGovernmentNumberWithoutCode(LocalGovernmentNumber):
 
     """C00003XXXXXXX　地方公共団体（団体コードなし）."""
 
-    def get_pattern(self):
-        pattern = super(LocalGovernmentNumberWithoutCode, self).get_pattern()
-        pattern[5] = 3
-        return pattern
+    @property
+    def pattern(self):
+        pattern = super(LocalGovernmentNumberWithoutCode, self).pattern.copy()
+        return pattern.update(5, 3)
 
 
 class CorporationNumber(LegalEntityNumber):
@@ -78,10 +80,11 @@ class OtherOrganizationNumber(LegalEntityNumber):
 
     """C7001XXXXXXXX　人格の無い団体等."""
 
-    def get_pattern(self):
-        pattern = super(OtherOrganizationNumber, self).get_pattern()
-        pattern[1] = 7
-        pattern[2] = 0
-        pattern[3] = 0
-        pattern[4] = 1
+    @property
+    def pattern(self):
+        pattern = super(OtherOrganizationNumber, self).pattern.copy()
+        pattern.update(1, 7)
+        pattern.update(2, 0)
+        pattern.update(3, 0)
+        pattern.update(4, 1)
         return pattern
